b58db260d6d84b258090c6cc82acbe04bac73146
JeanDelRosario <jeancarlos18_09@hotmail.com>
2018-06-17 20:01:57 -0400

    Update MS_PuestoBolsas.py

--- MS_PuestoBolsas.py
+++ MS_PuestoBolsas.py
@@ -13,10 +13,8 @@ import numpy as np
 excels = os.listdir('/home/jeancarlos/Desktop/InversionesReservas/Excels')
 
 columns_names = ['MS_RF_USD','MS_RF_USD_DOP','MS_RF_DOP','MS_RF_AC_MES','MS_RF_AC_ANO']
-puestos = ['ALPHA', 'ATLANBBA', 'BHDVAL','INRES', 'CCI','CITIV','EXCEL','IPSA',
-           'JMMB','PARVA','PCMDOM']
 
-inversiones2 = pd.DataFrame(columns=['puesto']+columns_names+['fecha_corte'])
+inversiones2 = pd.DataFrame(columns=['PUESTO']+columns_names+['FECHA_CORTE'])
 
 columns = []
 #Competidores
@@ -32,24 +30,34 @@ for i in range(len(excels)):
     # subtract the number of rows to read from the total number of rows (and another 1 for the header)
     data = pd.read_excel(workbook, sheet_name = 'BB_RFMSVTPBolsa')
     
-    row = np.where(data.iloc[:,0].str.contains('ALPHA|ATLANBBA|BHDVAL|INRES|CCI|CITIV|EXCEL|IPSA|JMMB|PARVA|PCMDOM', na=False))[0]
+    row = np.where(data.iloc[:,0].str.contains('Participante|Total', na=False))[0]
     
-    puestos_bolsa = list(data.iloc[row,0])
+    #Lo siguiente es para elegir siempre las filas que solo contengan informaciones de los puestos de bolsas
+    
+    #Primero verificamos si el documento tiene la estructura esperada
+    #Esta es: dos filas con la palabra participante y una con total
+    if len(row) != 3:
+        print('El documento '+excels[i]+' tiene un error en su estructura')
+        continue
+    
+    #Se eligen las filas entre la segunda vez que aparece la palabra participante y la ultima que aparece total
+    row = list(range(row[1]+1,row[2]-1))
     
     data1 = data.iloc[row,[0,2,3,4,5,9]]
-    data1.columns = ['puesto'] + columns_names
-    data1['fecha_corte'] = excels[i][0:8]
+    data1.columns = ['PUESTO'] + columns_names
+    data1['FECHA_CORTE'] = excels[i][0:8]
 
     inversiones2 = pd.concat([inversiones2, data1], axis = 0)
     
     if i % 10 == 0:
         print(i)
     
-inversiones2_sorted = inversiones2.sort_values(['puesto','fecha_corte'])
+    
+inversiones2_sorted = inversiones2.sort_values(['PUESTO','FECHA_CORTE'])
 
-inversiones2_sorted['mes'] = inversiones2_sorted['fecha_corte'].str[0:2]
+inversiones2_sorted['MES'] = inversiones2_sorted['FECHA_CORTE'].str[0:2]
 
-inversiones2_sorted['MS_RF_DIARIO'] = inversiones2_sorted.groupby(['puesto','mes'])['MS_RF_AC_MES'].diff()
+inversiones2_sorted['MS_RF_DIARIO'] = inversiones2_sorted.groupby(['PUESTO','MES'])['MS_RF_AC_MES'].diff()
 
 inversiones2_sorted['MS_RF_DIARIO'][inversiones2_sorted['MS_RF_DIARIO'].isnull()]=inversiones2_sorted['MS_RF_AC_MES'][inversiones2_sorted['MS_RF_DIARIO'].isnull()]
 
